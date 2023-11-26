678a90b999fdea24cfe343ef32a0e92c4e4f20a1
L <Luen@users.noreply.github.com>
2023-11-27 03:29:58 +1300

    .

--- app/components/Publications.tsx
+++ app/components/Publications.tsx
@@ -4,11 +4,14 @@ export default function Publications() {
       <h2 className="mt-12 mb-12 text-3xl text-center dark:text-white">Publications</h2>
       <p className="mt-12 mb-12 text-xl text-center dark:text-white">
         To view a list of our publications, please visit the&nbsp;
-        <a href="https://scholar.google.com/citations?user=ynWS968AAAAJ&hl=en&oi=ao" target="_blank" rel="noopener noreferrer">publications page</a>.
+        <a href="https://scholar.google.com/citations?user=ynWS968AAAAJ&hl=en&oi=ao" target="_blank" rel="noopener noreferrer">
+          publications page
+        </a>.
       </p>
       <p className="mt-12 mb-12 text-xl text-center dark:text-white">
-        Jodie L. Rummer&apos;s Papers&nbsp;
-        <a href="https://scholar.google.com/citations?user=ynWS968AAAAJ&hl=en&oi=ao" target="_blank" rel="noopener noreferrer">Google Scholar profile</a>.
+        <a href="https://scholar.google.com/citations?user=ynWS968AAAAJ&hl=en&oi=ao" target="_blank" rel="noopener noreferrer">
+          Jodie L. Rummer&apos;s Papers - Google Scholar profile
+        </a>.
       </p>
     </>
   );
