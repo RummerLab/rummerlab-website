39f5730253cddf2037fcfc6c25d4e15fff11b7e2
L <Luen@users.noreply.github.com>
2024-01-24 15:44:49 +1000

    .

--- app/api/publications/route.ts
+++ app/api/publications/route.ts
@@ -1,8 +1,13 @@
 import { NextResponse } from 'next/server';
+import { Scholar } from '@/lib/scholarly';
 
 export async function GET(req: any, res: any) {
   try {
-    const papers = {}
+    const { query } = req;
+    const { author } = query;
+    const scholar = new Scholar();
+    scholar.init();
+    const papers = await scholar.searchAuthor(author);
     return NextResponse.json(papers);
   } catch (error) {
     console.error(error);
