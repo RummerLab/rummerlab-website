aeeead9bb9949900b01a0fce54eb0ac1d3bcc553
L <luenwarneke@gmail.com>
2024-02-29 14:17:52 +1000

    Change wave to white bg

--- components/WavyHero.tsx
+++ components/WavyHero.tsx
@@ -5,10 +5,10 @@ import { WavyBackground } from "./ui/wavy-background";
 export function WavyHero() {
   return (
     <WavyBackground className="max-w-4xl mx-auto pb-20">
-      <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+      <p className="text-2xl md:text-4xl lg:text-7xl text-gray-800 font-bold inter-var text-center">
         Rummerlab
       </p>
-      <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
+      <p className="text-base md:text-lg mt-4 text-gray-800 font-normal inter-var text-center">
         RummerLab is a not-for-profit organisation
       </p>
     </WavyBackground>
