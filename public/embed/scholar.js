c68f07a96616bde24e9892b5c2238bd4ff11133c
L <luenwarneke@gmail.com>
2024-04-11 15:16:34 +1000

    Revamp embed code

--- public/embed/scholar.js
+++ public/embed/scholar.js
@@ -25,6 +25,13 @@ class ScholarEmbed {
             }
 
             const data = await response.json()
+
+            // Store the last modified date
+            const lastModified = response.headers.get('Last-Modified')
+            data.lastModified = lastModified
+                ? new Date(lastModified).toLocaleDateString()
+                : new Date().toLocaleDateString()
+
             return data
         } catch (error) {
             console.error('Error fetching publications:', error)
@@ -34,7 +41,7 @@ class ScholarEmbed {
     formatPublicationCitation(data) {
         const { bib, pub_url, num_citations } = data
 
-        const citationElement = document.createElement('div')
+        const container = document.createElement('div')
 
         if (bib.author) {
             const authorsFormatted = bib.author
@@ -47,24 +54,24 @@ class ScholarEmbed {
                 .join(', ')
 
             const authorsText = document.createTextNode(authorsFormatted + ' ')
-            citationElement.appendChild(authorsText)
+            container.appendChild(authorsText)
         }
         // Add publication year
         if (bib.pub_year) {
             const pubYearText = document.createTextNode(`(${bib.pub_year}) `)
-            citationElement.appendChild(pubYearText)
+            container.appendChild(pubYearText)
         }
 
         // Add title
         if (bib.title) {
             const titleText = document.createTextNode(`${bib.title}. `)
-            citationElement.appendChild(titleText)
+            container.appendChild(titleText)
         }
 
         // Add journal
         if (bib.journal) {
             const journalText = document.createTextNode(`${bib.journal}, `)
-            citationElement.appendChild(journalText)
+            container.appendChild(journalText)
         }
 
         // Add volume and number
@@ -74,13 +81,13 @@ class ScholarEmbed {
                     bib.number ? `(${bib.number}),` : ''
                 } `
             )
-            citationElement.appendChild(volumeNumberText)
+            container.appendChild(volumeNumberText)
         }
 
         // Add pages
         if (bib.pages) {
             const pagesText = document.createTextNode(`${bib.pages}. `)
-            citationElement.appendChild(pagesText)
+            container.appendChild(pagesText)
         }
 
         // Add citation count
@@ -88,7 +95,7 @@ class ScholarEmbed {
             const citationsText = document.createTextNode(
                 `(${num_citations} citations) `
             )
-            citationElement.appendChild(citationsText)
+            container.appendChild(citationsText)
         }
 
         // Add URL as a hyperlink
@@ -97,23 +104,48 @@ class ScholarEmbed {
             urlElement.href = pub_url
             urlElement.textContent = '[link]'
             urlElement.target = '_blank' // Open in new tab
-            citationElement.appendChild(urlElement)
+            container.appendChild(urlElement)
         }
 
-        return citationElement
+        return container
     }
 
     generateProfileSummary(data) {
-        const { name, citedby: publicationsCitations, hindex, i10index } = data
+        const {
+            lastModified,
+            name,
+            affiliation,
+            organization,
+            citedby,
+            publications,
+            hindex,
+            i10index,
+        } = data
+
+        // Publications count
+        const publicationsCount = publications ? publications.length : 0
 
         // Create container element for the profile summary
         const container = document.createElement('div')
 
+        const tile = document.createElement('h2')
+        tile.textContent = name
+        container.appendChild(tile)
+
+        const subtile = document.createElement('h3')
+        //const organizationLink = document.createElement('a')
+        //organizationLink.href = `https://scholar.google.com/citations?view_op=view_org&hl=en&org=${organization}`
+        //organizationLink.textContent = affiliation
+        //organizationLink.target = '_blank'
+        //subtile.appendChild(organizationLink)
+        subtile.textContent = affiliation
+        container.appendChild(subtile)
+
         // Create and append the Google Scholar profile link
         // Create the h2 element for the heading
-        const heading = document.createElement('h3')
+        const heading = document.createElement('p')
         // Create the text node for the non-link part of the heading
-        const textNode = document.createTextNode('Click here for my ')
+        /*const textNode = document.createTextNode('Click here for my ')
         // Append the text node to the h2 element
         heading.appendChild(textNode)
         // Create and configure the link for the Google Scholar profile part
@@ -122,48 +154,78 @@ class ScholarEmbed {
         scholarLink.textContent = 'Google Scholar profile'
         scholarLink.target = '_blank'
         // Append the link to the h2 element
-        heading.appendChild(scholarLink)
+        heading.appendChild(scholarLink)*/
         // Append the h2 element to the container
         container.appendChild(heading)
 
         // Add the publications text
-        const publicationsText = document.createElement('p')
-        publicationsText.textContent = `Publications: ${publicationsCitations} citations, h-index = ${hindex}, i10-index = ${i10index}; Google Scholar on ${new Date().toLocaleDateString()}`
-        container.appendChild(publicationsText)
-
-        // Add a line break
-        container.appendChild(document.createElement('br'))
-
-        // Add the summary text
+        const profileContainer = document.createElement('div')
+        profileContainer.className = 'profile-summary'
         const summaryText = document.createElement('p')
-        summaryText.textContent =
-            'Summary: Book chapters, journal articles, conference proceedings, editorial commentaries/perspectives'
-        container.appendChild(summaryText)
-
-        // Append line breaks and horizontal line for spacing
-        container.appendChild(document.createElement('br'))
-        container.appendChild(document.createElement('hr'))
-        container.appendChild(document.createElement('br'))
+        summaryText.textContent = `${publicationsCount} publications, ${citedby} citations, h-index: ${hindex}, i10-index: ${i10index}`
+        profileContainer.appendChild(summaryText)
+        const collaborators = this.generateCollaborators(data)
+        profileContainer.appendChild(collaborators)
+        const fetchDataInfo = document.createElement('p')
+        fetchDataInfo.textContent = 'Data fetched from '
+        const scholarLink = document.createElement('a')
+        scholarLink.href = `https://scholar.google.com/citations?user=${this.scholarId}`
+        scholarLink.textContent = 'Google Scholar'
+        scholarLink.target = '_blank'
+        fetchDataInfo.appendChild(scholarLink)
+        fetchDataInfo.textContent += ` on ${lastModified}`
+        profileContainer.appendChild(fetchDataInfo)
+        // Append the profile summary to the main container
+        container.appendChild(profileContainer)
 
         // The container has all the elements safely added.
         return container
     }
-    displayScholar(scholarData) {
-        this.container.innerHTML = '' // Clear existing content
 
-        const profileSummary = this.generateProfileSummary(scholarData)
-        const publicationData = scholarData.publications
+    generateCollaborators(data) {
+        const coAuthors = data.coauthors
+        const collaboratorNames = coAuthors.map((author) => author.name)
+        const container = document.createElement('p')
+        container.textContent = `Collaborator: ${collaboratorNames.join('')}`
+        if (collaboratorNames.length > 1) {
+            // Separate all but the last collaborator with commas, and add "and" before the last one
+            const allButLast = collaboratorNames.slice(0, -1).join(', ')
+            const last = collaboratorNames[collaboratorNames.length - 1]
+            container.textContent = `Collaborators: ${allButLast}, and ${last}`
+        }
 
+        return container
+    }
+
+    generatePublicationCitation(data) {
+        const container = document.createElement('p')
+        const publicationData = data.publications
         if (publicationData) {
             publicationData.sort((a, b) => b.bib.pub_year - a.bib.pub_year)
 
             publicationData.forEach((publication) => {
                 const formattedCitation =
                     this.formatPublicationCitation(publication)
-                this.container.appendChild(formattedCitation)
-                this.container.appendChild(document.createElement('br'))
+                container.appendChild(formattedCitation)
+                container.appendChild(document.createElement('br'))
             })
         }
+        return container
+    }
+
+    displayScholar(scholarData) {
+        this.container.innerHTML = '' // Clear any existing content
+
+        const profileSummary = this.generateProfileSummary(scholarData)
+        this.container.appendChild(profileSummary)
+
+        this.container.appendChild(document.createElement('br'))
+        this.container.appendChild(document.createElement('hr'))
+        this.container.appendChild(document.createElement('br'))
+
+        const publicationCitation =
+            this.generatePublicationCitation(scholarData)
+        this.container.appendChild(publicationCitation)
     }
 }
 
