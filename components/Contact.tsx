16dee5fff8689646d8bf08d703cc578651ecaa97
L <Luen@users.noreply.github.com>
2024-07-08 15:28:16 +1000

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -3,7 +3,7 @@ export default function Contact() {
     <div className="">
         <h2 className="mt-12 mb-12 text-3xl text-center">Contact Us</h2>
         <p className="mt-12 mb-12 text-xl text-center">
-          If you want to reach out, please feel free
+          Feel free
           to reach out at <a href="mailto:jodierummer@rummerlab.com" className="text-blue-500 hover:underline cursor-pointer">jodierummer@rummerlab.com</a>.
         </p>
     </div>
